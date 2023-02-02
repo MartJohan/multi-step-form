be15d9d2bb7fe0fe5ea10ba68b2c88c075e6d903
Martin <martin.johansen99@hotmail.com>
2023-02-02 12:11:25 +0100

    Fixed button placement and logic

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,26 @@
-<script setup lang="ts">
+<script setup lang="ts">import { selectedStepKey } from '@/keys/keys';
+import type { TSelectedStep } from '@/types/selectedStep';
+import { inject } from 'vue';
+
+
+const step = inject<TSelectedStep>(selectedStepKey);
+
 </script>
 
 <template>
-    <div class="h-full w-full overflow-hidden flex flex-row justify-between items-center p-2 font-semibold">
-        <div class="p-2">
+    <div 
+    v-if="step"
+    :class="[step.selectedStep.value > 1 ? 'justify-between' : 'justify-end']"
+    class="h-full w-full overflow-hidden flex flex-row items-center p-2 font-semibold">
+        <div 
+        class="p-2"
+        :class="[step.selectedStep.value > 1 ? 'block' : 'hidden']">
             <button class="rounded p-2 text-coolGray self-start">
                 Go Back
             </button>
         </div>
         <div class="p-2">
-            <button class="rounded p-2 bg-marineBlue text-white self-end">
+            <button class="rounded p-2 bg-marineBlue text-white">
                 Next Step
             </button>
         </div>

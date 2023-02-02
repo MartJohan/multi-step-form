26b51b3b5337474b646406eb0b6b750b61c38a0c
Martin <martin.johansen99@hotmail.com>
2023-02-02 10:30:01 +0100

    Changed text color on selected node

--- src/components/Step-banner-steps.vue
+++ src/components/Step-banner-steps.vue
@@ -12,9 +12,10 @@ const step = inject<TSelectedStep>(selectedStepKey);
     <div class="h-fit w-full flex flex-row gap-x-5 justify-center">
         <div 
         v-for="(number, index) in step?.stepAmount"
-        :class="[number === step?.selectedStep.value ? 'bg-pastelBlue text-black' : '']"
+        :class="[number === step?.selectedStep.value ? 'bg-pastelBlue' : '']"
         class="border border-white rounded-full h-8 w-8 text-center align-middle leading-8 font-medium text-white">
-                <p>
+                <p 
+                :class="[number === step?.selectedStep.value ? 'text-black' : '']">
                     {{ number }}
                 </p>
         </div>

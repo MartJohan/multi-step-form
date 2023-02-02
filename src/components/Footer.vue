248649e289cf1fd9a2455077461ee08bffe2ad2c
Martin <martin.johansen99@hotmail.com>
2023-02-02 10:45:19 +0100

    Added footer styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,16 @@
 </script>
 
 <template>
-    <div class="h-full w-full overflow-hidden">
-        <div class="text-xl">footer</div>
+    <div class="h-full w-full overflow-hidden flex flex-row justify-between items-center p-2 font-semibold">
+        <div class="p-2">
+            <button class="rounded p-2 text-coolGray self-start">
+                Go Back
+            </button>
+        </div>
+        <div class="p-2">
+            <button class="rounded p-2 bg-marineBlue text-white self-end">
+                Next Step
+            </button>
+        </div>
     </div>
 </template>

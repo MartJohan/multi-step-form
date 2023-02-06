6eba295e1217f0fd0cf6c0198f86671c732538f5
Martin <martin.johansen99@hotmail.com>
2023-02-06 12:35:45 +0100

    Added a seperate translated component to serve as a container for all others

--- src/components/main/Main-View.vue
+++ src/components/main/Main-View.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { selectedStepKey } from '@/keys';
+import type { TSelectedStep } from '@/types';
+import { inject } from 'vue';
 import PersonalInfo from './persona/Personal-Info.vue'
+import PlanView from './plan/Plan-View.vue'
 import TranslatedContainer from './Translated-Container.vue'
+
+const step = inject<TSelectedStep>(selectedStepKey);
 
 
 </script>
@@ -8,8 +14,8 @@ import TranslatedContainer from './Translated-Container.vue'
 <template>
     <div class="h-full w-full overflow-hidden bg-lightGray bg-opacity-25">
         <TranslatedContainer>
-            <PersonalInfo />
-
+            <PersonalInfo  v-if="step?.selectedStep.value === 1"/>
+            <PlanView v-if="step?.selectedStep.value === 2"/>
         </TranslatedContainer>
     </div>
 </template>
--- src/components/main/Translated-Container.vue
+++ src/components/main/Translated-Container.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="bg-white -translate-y-24 z-10 absolute rounded w-fit p-4 m-4">
+    <div class="bg-white -translate-y-16 z-10 absolute rounded w-fit p-4 m-4 drop-shadow-lg">
         <slot />
     </div>
 </template>

50bd6157374437b3ae533521a1cfda059e77cd60
Martin <martin.johansen99@hotmail.com>
2023-02-08 10:21:20 +0100

    style change

--- src/components/main/Translated-Container.vue
+++ src/components/main/Translated-Container.vue
@@ -11,7 +11,7 @@ const props = defineProps<TInputProps>();
 </script>
 
 <template>
-    <div class="bg-white -translate-y-16 absolute rounded w-fit m-4 drop-shadow-lg z-40">
+    <div class="bg-white -translate-y-16 absolute rounded-lg w-fit m-4 px-2 drop-shadow-lg z-40">
         <div class="w-full h-full flex flex-col p-4">
             <h1 class="text-marineBlue font-bold text-xl">
                 {{ props.title }}

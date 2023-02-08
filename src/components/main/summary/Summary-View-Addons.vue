98d3f348cd40b18d3d9d1ba99350f828c2a2c1a5
Martin <martin.johansen99@hotmail.com>
2023-02-08 12:08:07 +0100

    Added summary component data

---
+++ src/components/main/summary/Summary-View-Addons.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { TAddons } from '@/types';
+
+
+type TSummaryAddonsProps = {
+    addon: TAddons;
+    showMonthly: boolean;
+}
+
+defineProps<TSummaryAddonsProps>()
+
+</script>
+
+<template>
+    <div class="flex flex-row justify-between text-sm p-2 text-coolGray">
+        <p>{{ addon.title }}</p>
+        <p class="text-marineBlue">
+            +${{ showMonthly ? addon.monthlyPrice + '/mo' : addon.yearlyPrice + 'yr'}}
+        </p>
+    </div>
+</template>
--- src/components/main/summary/Summary-View.vue
+++ src/components/main/summary/Summary-View.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
+import { selectedAddonsKey, selectedMonthlyPlanKey, selectedPlanKey, sumKey } from '@/keys';
+import type { TAddonsProvider, TPlanProvider, TSelectedMonthlyPlanProvider, TSumProvider } from '@/types';
+import SummaryViewAddons from './Summary-View-Addons.vue'
+import { inject } from 'vue';
+
+
+const selectedPlan = inject<TPlanProvider>(selectedPlanKey)!;
+const selectedMonthlyPlan = inject<TSelectedMonthlyPlanProvider>(selectedMonthlyPlanKey)!;
+const selectedAddons  = inject<TAddonsProvider>(selectedAddonsKey)!;
+const sum = inject<TSumProvider>(sumKey);
 
 </script>
 
 <template>
+    <div class="flex flex-col">
+        <div class="bg-magnolia flex flex-col rounded-lg divide-y divide-coolGray divide-opacity-50 p-2">
+            <div class="flex flex-row justify-between text-sm">
+                <div class="flex flex-col p-2">
+                    <p class="text-marineBlue font-semibold">
+                        {{ selectedPlan.selectedPlan.value.name }}
+                        <span>{{ selectedMonthlyPlan.selectedMonthlyPlan.value ? '(Monthly)' : '(Yearly)' }}</span>
+                    </p>
+                    <p class="text-coolGray underline">Change</p>
+                </div>
+                <p class="p-2 text-marineBlue font-semibold">
+                    {{ selectedMonthlyPlan.selectedMonthlyPlan.value ? selectedPlan.selectedPlan.value.monthlyPrice + '/mo' 
+                    : selectedPlan.selectedPlan.value.yearlyPrice + '/yr' }}
+                </p>
+            </div>
+            <div class="flex flex-col">
+                <SummaryViewAddons 
+                v-if="selectedAddons.selectedAddons.value.length" 
+                v-for="addon in selectedAddons.selectedAddons.value" 
+                :addon="addon" :show-monthly="selectedMonthlyPlan.selectedMonthlyPlan.value" />
+                <p v-if="!selectedAddons.selectedAddons.value.length" class="w-full text-center p-2">
+                    No addons chosen
+                </p>
 
-    
-
+            </div>
+        </div>
+        <div class="flex flex-row p-2 my-2 text-coolGray justify-between">
+            <p>
+                Total <span>{{ selectedMonthlyPlan.selectedMonthlyPlan.value ? '(per month)' : '(per year)' }}</span>
+            </p>
+            <p class="text-purplishBlue font-semibold">
+                +${{ selectedMonthlyPlan.selectedMonthlyPlan.value 
+                ? sum?.getMonthlySum() + '/mo'
+                : sum?.getYearlySum() + '/yr' }}
+            </p>
+        </div>
+    </div>
 </template>

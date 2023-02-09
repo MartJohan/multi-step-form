fc43de495da97fc405d8717f1376a7b42228f538
Martin <martin.johansen99@hotmail.com>
2023-02-09 10:11:47 +0100

    Desktop styling for banner and banner steps

--- src/components/header/Step-banner-steps.vue
+++ src/components/header/Step-banner-steps.vue
@@ -6,20 +6,31 @@ import { selectedStepKey } from '../../keys/keys'
 
 const step = inject<TSelectedStepProvider>(selectedStepKey);
 
+const stepName = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"]
+
 </script>
 
 <template>
-    <div class="h-fit w-full flex flex-row gap-x-5 justify-center">
+    <div class="h-fit w-full flex flex-row gap-x-5 justify-center xl:flex-col xl:gap-y-10 xl:my-2">
         <div 
         v-for="(number, index) in step?.stepAmount"
-        :class="[number === step?.selectedStep.value ? 'bg-pastelBlue' : '']"
-        class="border border-white rounded-full h-8 w-8 text-center align-middle leading-8 font-medium text-white">
+        class=" text-center align-middle leading-8 font-medium text-white 
+        xl:flex xl:flex-row xl:items-center xl:gap-x-4">
                 <p 
-                :class="[number === step?.selectedStep.value ? 'text-black' : '']"
-                class="font-semibold"
+                :class="[number === step?.selectedStep.value ? 'text-black bg-pastelBlue' : '', ]"
+                class="font-semibold border rounded-full h-8 w-8"
                 >
                     {{ number }}
                 </p>
+                <div class="hidden xl:flex xl:flex-col xl:justify-start">
+                    <p class="text-coolGray w-fit">
+                        Step {{ step?.selectedStep.value }}
+                    </p>
+                    <p class="text-sm font-semibold w-fit">
+                        {{ stepName[step?.selectedStep.value!] }}
+                    </p>
+            </div>
         </div>
+        
     </div>
 </template>
--- src/components/header/Step-banner.vue
+++ src/components/header/Step-banner.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import BGSidebarMobile from '@/assets/images/bg-sidebar-mobile.svg';
+import BGSidebarDesktop from '@/assets/images/bg-sidebar-desktop.svg'
+import { ref } from 'vue';
 import StepBannerSteps from './Step-banner-steps.vue';
+
+const screenWidth = ref(window.innerWidth);
+
 </script>
 
 <template>
     <div class="h-full w-full overflow-hidden -z-10 relative">
         <div 
-        :style="{ backgroundImage: `url(${BGSidebarMobile})` }" 
-        class="h-full w-full flex bg-cover bg-no-repeat">
+        :style="{ backgroundImage: screenWidth < 1280 ? `url(${BGSidebarMobile})` : `url(${BGSidebarDesktop})` }" 
+        class="h-full w-full flex bg-cover bg-no-repeat xl:bg-contain">
             <StepBannerSteps class="p-6"></StepBannerSteps>
         </div>
     </div>

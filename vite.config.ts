e16ce6c6fb9a6ed9b341eba27c2edcde7fae5902
Martin <martin.johansen99@hotmail.com>
2023-02-14 07:31:47 +0100

    Update of dpelo shell and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MartJohan/multi-step-form',
+  base: '/multi-step-form/',
   plugins: [vue()],
   resolve: {
     alias: {
